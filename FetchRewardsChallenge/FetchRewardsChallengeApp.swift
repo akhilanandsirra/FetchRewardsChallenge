be6d104ba42d1155a4b800175790497b3925bb0b
akhilanandsirra <akhilanand9999@gmail.com>
2023-11-01 22:16:14 -0400

    Initial Commit

---
+++ FetchRewardsChallenge/FetchRewardsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRewardsChallengeApp.swift
+//  FetchRewardsChallenge
+//
+//  Created by Akhil Anand Sirra on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchRewardsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRewardsChallengeUITests/FetchRewardsChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FetchRewardsChallengeUITestsLaunchTests.swift
+//  FetchRewardsChallengeUITests
+//
+//  Created by Akhil Anand Sirra on 01/11/23.
+//
+
+import XCTest
+
+final class FetchRewardsChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
